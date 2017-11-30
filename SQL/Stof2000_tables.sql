cc07457100edf7d330f2a4d81059de8332ce83d5
Daniel Stuhr Petersen <dani7354@edu.eal.dk>
2017-11-30 16:48:58 +0100

    (SQL) opret produkt-, farve- samt stoftypetabel

---
+++ SQL/Stof2000_tables.sql
@@ -0,0 +1,31 @@
+USE DB2017_CXX
+
+CREATE TABLE PRODUCT_STOF2000 (
+	Varenummer		Int		NOT NULL,
+	Varenavn		nvarchar(MAX)	NOT NULL DEFAULT 'Ukendt',
+	Beskrivelse		nvarchar(MAX)	NOT NULL DEFAULT 'Der findes ingen beskrivelse for denne vare.',
+	PrisPrM			float			NULL,
+	MængdeM			float			NOT NULL DEFAULT 0, 
+	Stoftype		Int				NULL,
+	Farve			Int				NULL, 
+	Mønster			nvarchar(MAX)	NULL,
+	CONSTRAINT		PRODUCT_STOF2000_PK		PRIMARY KEY(Varenummer),
+	CONSTRAINT		PRODUCT_STOF2000_FK		FOREIGN KEY(Stoftype)
+			REFERENCES		STOFTYPE(StofID)
+			ON UPDATE CASCADE, 
+	CONSTRAINT		PRODUCT_STOF2000_FK2	FOREIGN KEY(Farve)
+			REFERENCES		FARVE(FarveID)
+			ON UPDATE CASCADE
+);
+
+CREATE TABLE STOFTYPE (
+	StofID		Int		NOT NULL IDENTITY(1, 1),
+	StoftypeNavn		nvarchar(MAX)		NOT NULL DEFAULT 'Ukendt',
+	CONSTRAINT		STOFTYPE_PK		PRIMARY KEY(StofID)
+);
+
+CREATE TABLE FARVE(
+	FarveID		int 	NOT NULL IDENTITY(1, 1),
+	Farvenavn	nvarchar(MAX)	NOT NULL DEFAULT 'Ukendt', 
+	CONSTRAINT		FARVE_PK	PRIMARY KEY(FarveID)
+);
